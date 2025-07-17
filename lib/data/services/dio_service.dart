81c53e89a5ebc73fde0c773c6d5c42d4a53be77e
Tolga Direk <103821299+tolgadirek@users.noreply.github.com>
2025-07-17 21:48:28 +0300

    Deleted api_key.dart

--- lib/data/services/dio_service.dart
+++ lib/data/services/dio_service.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 
-const String accessToken = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MmY3MzQ0ODYwMzlhNTEyMGExYmM4MzhmYjE4ODhiMyIsIm5iZiI6MTc1MjY0OTk4OC4wNCwic3ViIjoiNjg3NzUxMDRiODdhMzZlYTIxY2NlMTBhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.FgZRLgn9ysSc5OJE_Pbi9kdGt1BhQACLRgjebqHES6I';
+const String accessToken = 'Bearer API_KEY';
 
 final Dio dio = Dio(
   BaseOptions(
